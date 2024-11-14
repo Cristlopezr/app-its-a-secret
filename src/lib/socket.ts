3cc134322f401194a00e30177b7eb45f189f6600
cristian <cristianlopezr.931@gmail.com>
2024-11-14 15:36:20 -0300

    Deleted some console logs

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,5 +4,4 @@ import { io } from 'socket.io-client';
 /* export const socket = io('http://localhost:3001'); */
 
 //Production
-console.log('URL ', process.env.NEXT_PUBLIC_WSS_URL);
 export const socket = io(process.env.NEXT_PUBLIC_WSS_URL, { transports: ['websocket'] });
